d6527815094cd392d7b1127e8101004ad3ab7ebe
langdon <langdon@fedoraproject.org>
2016-05-03 23:10:29 -0400

    new code, fixes to vagrantfile, note: vagrantfile gets wonky because vagrant uid = 1001 vs fedora default 1000 so nfs is wacky

--- Vagrantfile
+++ Vagrantfile
@@ -4,11 +4,13 @@ Vagrant.configure(2) do |config|
     vagrant_host.vm.synced_folder "/home/lwhite/loc-projects", "/mnt/host-projects", type: "nfs"
     vagrant_host.vm.network "forwarded_port", guest: 3000, host: 3000
 
-    
     vagrant_host.vm.provision "shell", inline: "sudo dnf install -y nodejs npm "
+    #vagrant_host.vm.provision "shell", inline: "sudo usermod -u 1000 vagrant"
+    #vagrant_host.vm.provision "shell", inline: "sudo groupmod -g 1000 vagrant"
     vagrant_host.vm.provision "shell", inline: "ln -sf /mnt/host-projects/player-cert /home/vagrant/ || :"
     vagrant_host.vm.provision "shell", inline: "mkdir /home/vagrant/player-cert/src/player-cert/data || :"
     vagrant_host.vm.provision "shell", inline: "touch  /home/vagrant/player-cert/src/player-cert/data/player-cert.db"
-    vagrant_host.vm.provision "shell", inline: "chown -R vagrant:vagrant /home/vagrant/player-cert/src/player-cert/"
+    #vagrant_host.vm.provision "shell", inline: "chown -R vagrant:vagrant /home/vagrant/player-cert/src/player-cert/"
+    vagrant_host.vm.provision "shell", inline: "cd /home/vagrant/player-cert/src/player-cert && npm install"
     end
 end
--- src/player-cert/app.js
+++ src/player-cert/app.js
@@ -22,8 +22,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -59,6 +69,92 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
+var Sequelize = require('sequelize')
+  , sequelize = new Sequelize('player-cert', 'username', 'secret', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: './data/player-cert.sqlite'
+});
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  }, function (err) { 
+    console.log('Unable to connect to the database:', err);
+  });
+
+
+var Referee = sequelize.define('referees', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name'
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    field: 'last_name'
+  },
+  emailAddress: {
+    type: Sequelize.STRING,
+    field: 'email_address'
+  },
+  password: {
+    type: Sequelize.STRING,
+    field: 'password' 
+  },
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+var Player = sequelize.define('players', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name'
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    field: 'last_name'
+  },
+  emailAddress: {
+    type: Sequelize.STRING,
+    field: 'email_address'
+  },
+  password: {
+    type: Sequelize.STRING,
+    field: 'password' 
+  },
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+var Team = sequelize.define('teams', {
+  teamName: {
+    type: Sequelize.STRING,
+    field: 'team_name'
+  },
+  city: {
+    type: Sequelize.STRING,
+    field: 'city'
+  },
+  state: {
+    type: Sequelize.STRING,
+    field: 'state'
+  },
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+Player.belongsTo(Team);
+
+/*
 var fs = require("fs");
 var file = "./data/player-cert.db";
 //var file = "player-cert.db";
@@ -66,6 +162,17 @@ var exists = fs.existsSync(file);
 
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
+//create initial db if it doesn't exist
+db.serialize(function() {
+  if(!exists) {
+    console.log("did not exist")
+    db.run("CREATE TABLE Referees (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, email_address TEXT, password TEXT)");
+    db.run("CREATE TABLE Players (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, email_address TEXT, password TEXT)");
+    db.run("CREATE TABLE Teams (id INTEGER PRIMARY KEY, name TEXT, city TEXT, state TEXT)");
+    db.run("CREATE TABLE Team_Players (id INTEGER PRIMARY KEY, player_id INTEGER, team INTEGER)");
+  }
+});
+*/
 
 var msg = "hello world";
 console.log(msg)
